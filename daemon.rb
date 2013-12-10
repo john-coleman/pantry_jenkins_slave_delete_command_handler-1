cce3713752615bc4880dfceb837d4be67e455340
John Coleman <john.coleman@wonga.com>
2013-12-10 10:24:03 +0000

    TD-1613 add chef_configure in daemon.rb

--- daemon.rb
+++ daemon.rb
@@ -5,4 +5,5 @@ require_relative 'pantry_jenkins_slave_delete_command_handler/pantry_jenkins_sla
 
 config_name = File.join(File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__), "config", "daemon.yml")
 Wonga::Daemon.load_config(File.expand_path(config_name))
+Chef::Knife.new.configure_chef
 Wonga::Daemon.run(Wonga::Daemon::PantryJenkinsSlaveDeleteCommandHandler.new(Wonga::Daemon.publisher, Wonga::Daemon.logger))
